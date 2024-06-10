48615b720ac4c93e706fc5782b11fa915d0c3b43
Daniel Alves <daniel.zebigodegithub@gmail.com>
2024-06-10 16:58:04 -0300

    crei as funcionalidades
    criei todas funcionalidades ver livro e baixar livro

--- bibliotecaWeb/src/main/java/com/curso/bibliotecaWeb/controller/LivroController.java
+++ bibliotecaWeb/src/main/java/com/curso/bibliotecaWeb/controller/LivroController.java
@@ -1,12 +1,27 @@
 package com.curso.bibliotecaWeb.controller;
 
+import com.curso.bibliotecaWeb.model.Livro;
+import com.curso.bibliotecaWeb.repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
 @Controller
 public class LivroController {
+
+    @Autowired
+    private LivroRepository livroRepository;
 
     @GetMapping("/")
     public String index() {
@@ -14,10 +29,37 @@ public class LivroController {
     }
 
     @GetMapping("/detalhes")
-    public String detalhes(@RequestParam String titulo, @RequestParam String autor, @RequestParam String ano, Model model) {
-        model.addAttribute("titulo", titulo);
-        model.addAttribute("autor", autor);
-        model.addAttribute("ano", ano);
+    public String detalhes(@RequestParam Long id, Model model) {
+        Livro livro = livroRepository.findById(id).orElse(null);
+        model.addAttribute("livro", livro);
         return "detalhes";
+    }
+
+    @GetMapping("/pesquisarLivro")
+    @ResponseBody
+    public List<Livro> pesquisarLivro(@RequestParam String search) {
+        return livroRepository.findByTituloContainingOrAutorContaining(search, search);
+    }
+
+    @GetMapping("/baixar")
+    public ResponseEntity<byte[]> download(@RequestParam Long id) {
+        Livro livro = livroRepository.findById(id).orElse(null);
+        if (livro == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        String bookDetails = "Título: " + livro.getTitulo() + "\n" +
+                             "Autor: " + livro.getAutor() + "\n" +
+                             "Texto: " + livro.getTexto();
+
+        byte[] bookBytes = bookDetails.getBytes(StandardCharsets.UTF_8);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        headers.setContentDispositionFormData("attachment", "Livro-"+livro.getTitulo()+".txt");
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(bookBytes);
     }
 }
--- bibliotecaWeb/src/main/resources/static/js/script.js
+++ bibliotecaWeb/src/main/resources/static/js/script.js
@@ -1,7 +1,37 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(document).ready(function() {
+    $('#searchButton').click(function() {
+        pesquisarLivro();
+    });
+
+    $('#searchInput').on('input', function() {
+        pesquisarLivro();
+    });
+    
+    
+});
+
+function voltar() {
+        window.history.back();
+    }
+
+function pesquisarLivro() {
+    var input = $('#searchInput').val();
+    var url = '/pesquisarLivro?search=' + input;
+    $.get(url, function(data) {
+        var tableBody = $('#resultTable tbody');
+        tableBody.empty();
+        $.each(data, function(index, livro) {
+            var newRow = $('<tr>');
+            newRow.append($('<td>').text(livro.titulo));
+            newRow.append($('<td>').text(livro.autor));
+            newRow.append($('<td>').html(
+                '<a href="/detalhes?id=' + livro.id + '" class="btn btn-info me-2">Ver Livro</a>' +
+                '<a href="/baixar?id=' + livro.id + '" class="btn btn-success">Baixar Livro</a>'
+            ));
+            tableBody.append(newRow);
+        });
+    });
+}
+
 
 
